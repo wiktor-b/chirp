c4896d3800fbac7b2f8caeed59b9816691d1c3de
wiktor-b <wiktor2306@gmail.com>
2023-06-19 13:29:21 +0200

    Profile page with ssg helper

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -16,8 +16,20 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        {" "}
-        <div> {data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">
+          {`@${data.username ?? ""}`}
+        </div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
@@ -28,6 +40,7 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
 import SuperJSON from "superjson";
 import { PageLayout } from "~/components/layout";
+import Image from "next/image";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const helpers = createServerSideHelpers({
